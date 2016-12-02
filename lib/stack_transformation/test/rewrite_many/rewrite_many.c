db67ec697c331f97d2f7f22b4edc0da9ccb934dd
Rob Lyerly <rlyerly@vt.edu>
2016-12-02 14:08:03 -0500

    Fixed data type bug in stack transformation test case

--- lib/stack_transformation/test/rewrite_many/rewrite_many.c
+++ lib/stack_transformation/test/rewrite_many/rewrite_many.c
@@ -24,9 +24,9 @@ long outer_frame()
 long recurse(int depth)
 {
   int a1, a2, a3, a4, a5, a6, a7, a8, a9,
-      a10, a11, a12, a13, a14, a15, a16, ret;
+      a10, a11, a12, a13, a14, a15, a16;
   long b1, b2, b3, b4, b5, b6, b7, b8,
-       b9, b10, b11, b12, b13, b14, b15, b16;
+       b9, b10, b11, b12, b13, b14, b15, b16, ret;
   
 
   a1 = rand() % 8;
