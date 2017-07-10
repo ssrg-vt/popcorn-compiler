a807ad6c63f1d36ddd5dd966ad98f9d592859883
Pierre Olivier <polivier@vt.edu>
2017-07-10 17:29:57 -0400

    More cleanup

--- tool/alignment/src/Section.java
+++ tool/alignment/src/Section.java
@@ -1,53 +1,42 @@
 
 
-public class Section < X, Y, Z >
-{
-  public X x;			//name
-  public Y y;			// addr 
-  public Z z;			// size
+public class Section < X, Y, Z > {
+    public X x;			//name
+    public Y y;			// addr 
+    public Z z;			// size
 
 
-  public Section (X x, Y y, Z z)
-  {
-    this.x = x;
-    this.y = y;
-    this.z = z;
-  }
-
-   @Override public String toString ()
-  {
-    return "(" + x + ',' + y + ',' + z + ')';
-  }
+    public Section(X x, Y y, Z z) {
+	this.x = x;
+	this.y = y;
+	this.z = z;
+    } @Override public String toString() {
+	return "(" + x + ',' + y + ',' + z + ')';
+    }
 
   /************* Get ***************************/
-  public String getName ()
-  {
-    return (String) x;
-  }
+    public String getName() {
+	return (String) x;
+    }
 
-  public Y getAddr ()
-  {
-    return y;
-  }
+    public Y getAddr() {
+	return y;
+    }
 
-  public Z getSize ()
-  {
-    return z;
-  }
+    public Z getSize() {
+	return z;
+    }
 
   /************* Set ************************/
-  public void setName (X name)
-  {
-    this.x = name;
-  }
+    public void setName(X name) {
+	this.x = name;
+    }
 
-  public void setAddr (Y addr)
-  {
-    this.y = addr;
-  }
+    public void setAddr(Y addr) {
+	this.y = addr;
+    }
 
-  public void setSize (Z size)
-  {
-    this.z = size;
-  }
+    public void setSize(Z size) {
+	this.z = size;
+    }
 }
