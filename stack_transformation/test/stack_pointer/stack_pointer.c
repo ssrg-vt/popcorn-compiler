023a8affc41f599436a666351733d8b94438daa1
harubyy <akaharuneb@gmail.com>
2017-07-17 21:16:20 +0000

    Add files via upload

---
+++ stack_transformation/test/stack_pointer/stack_pointer.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <stack_transform.h>
+#include "stack_transform_timing.h"
+
+static int max_depth = 10;
+static int post_transform = 0;
+
+void outer_frame()
+{
+  if(!post_transform)
+  {
+#ifdef __aarch64__
+    TIME_AND_TEST_REWRITE("./stack_pointer_aarch64", outer_frame);
+#elif defined(__x86_64__)
+    TIME_AND_TEST_REWRITE("./stack_pointer_x86-64", outer_frame);
+#endif
+  }
+}
+
+void recurse(int depth, int* myvar)
+{
+  if(depth < max_depth) recurse(depth + 1, myvar);
+  else outer_frame();
+  (*myvar)++;
+}
+
+int main(int argc, char** argv)
+{
+  int myvar = 0;
+
+  if(argc > 1)
+    max_depth = atoi(argv[1]);
+
+  recurse(1, &myvar);
+  printf("%s: myvar = %d\n", argv[0], myvar);
+  return (myvar == 0 ? 1 : 0);
+}
+
