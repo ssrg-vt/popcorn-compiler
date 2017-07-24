f96e7301d09df642c4313308fd80d5bbbb980100
Rob Lyerly <rlyerly@vt.edu>
2017-07-24 11:30:26 -0400

    Equivalence point pass adds HTM stop/start around libc I/O functions

--- patches/llvm/src/lib/Transforms/Instrumentation/EquivalencePoints.cpp
+++ patches/llvm/src/lib/Transforms/Instrumentation/EquivalencePoints.cpp
@@ -23,6 +23,7 @@
 
 #include "llvm/Pass.h"
 #include "llvm/ADT/StringMap.h"
+#include "llvm/ADT/StringSet.h"
 #include "llvm/ADT/Triple.h"
 #include "llvm/IR/DiagnosticInfo.h"
 #include "llvm/IR/InlineAsm.h"
@@ -84,17 +85,17 @@ public:
   virtual bool doInitialization(Module &M) {
     // Make sure HTM is supported on this architecture if attempting to
     // instrument with transactional execution
-    doHTMAsmInstrumentation = false;
+    doHTMInstrumentation = false;
     if(HTMExec) {
       Triple Arch(M.getTargetTriple());
-      if(HTMAsm.find(Arch.getArchName()) == HTMAsm.end()) {
+      if(HTMEqPoint.find(Arch.getArchName()) == HTMEqPoint.end()) {
         std::string Msg("HTM instrumentation not supported for '");
         Msg += Arch.getArchName();
         Msg += "'";
         DiagnosticInfoInlineAsm DI(Msg, DiagnosticSeverity::DS_Warning);
         M.getContext().diagnose(DI);
       }
-      else doHTMAsmInstrumentation = true;
+      else doHTMInstrumentation = true;
     }
     return false;
   }
@@ -112,6 +113,10 @@ public:
       }
     }
 
+    // Some libc functions (e.g., I/O) will cause aborts from system calls.
+    // Instrument libc calls to stop & resume transactions afterwards.
+    if(doHTMInstrumentation) wrapLibcWithHTM(F);
+
     return numInstrumented > 0;
   }
 
@@ -122,16 +127,11 @@ private:
   /// Rather than modifying the command-line argument (which can mess up
   /// compile configurations for multi-ISA binary generation), store a
   /// per-module value during intialization
-  bool doHTMAsmInstrumentation;
+  bool doHTMInstrumentation;
 
   /// HTM inline assembly for a given architecture
-  struct HTMAsmSpec {
-    /// Assembly template (i.e., assembler instructions) for HTM stop/start.
-    /// Implements the following pseudo-code:
-    ///
-    ///   if(in_transaction):
-    ///     stop_transaction
-    ///   start_transaction
+  struct AsmSpec {
+    /// Assembly template, i.e., assembler instructions.
     std::string Template;
 
     /// Constraints (inputs, outputs, clobbers) for the assembly template
@@ -148,19 +148,27 @@ private:
   };
 
   /// Per-architecture inline assembly for HTM execution
-  const static StringMap<HTMAsmSpec> HTMAsm;
+  const static StringMap<AsmSpec> HTMBegin;
+  const static StringMap<AsmSpec> HTMEnd;
+  const static StringMap<AsmSpec> HTMEqPoint;
+
+  /// libc functions which are likely to cause an HTM abort through a syscall
+  // TODO LLVM has to have a better way to detect these
+  const static StringSet<> LibcIO;
 
   /// Get an architecture-specific inline ASM statement for transactional
-  /// execution at equivalence points
+  /// execution at equivalence points.  The template argument specifies which
+  /// assembly to generate, e.g., HTM begin/end, or HTM equivalence points.
+  template<const StringMap<AsmSpec> &Asm>
   static InlineAsm *getHTMAsm(const Module &M) {
-    StringMap<HTMAsmSpec>::const_iterator It;
+    StringMap<AsmSpec>::const_iterator It;
     Triple Arch(M.getTargetTriple());
 
     FunctionType *FuncTy =
       FunctionType::get(Type::getVoidTy(M.getContext()), false);
-    It = HTMAsm.find(Arch.getArchName());
-    assert(It != HTMAsm.end() && "Unsupported architecture");
-    const HTMAsmSpec &Spec = It->second;
+    It = Asm.find(Arch.getArchName());
+    assert(It != Asm.end() && "Unsupported architecture");
+    const AsmSpec &Spec = It->second;
     return InlineAsm::get(FuncTy,
                           Spec.Template,
                           Spec.Constraints,
@@ -173,10 +181,51 @@ private:
   void addEquivalencePoint(Instruction *I) {
     IRBuilder<> Worker(I);
 
-    if(doHTMAsmInstrumentation) Worker.CreateCall(getHTMAsm(*I->getModule()));
+    if(doHTMInstrumentation)
+      Worker.CreateCall(getHTMAsm<HTMEqPoint>(*I->getModule()));
     // TODO insert flag check & migration call if flag is set
 
     numInstrumented++;
+  }
+
+  /// Return whether the call instruction is a libc I/O call
+  static inline bool isLibcIO(const Instruction *I) {
+    const CallInst *CI;
+
+    // Is it a call instruction?
+    if(!I || !isa<CallInst>(I)) return false;
+    CI = cast<CallInst>(I);
+
+    // Is the called function in the set of libc I/O functions?
+    const Function *CalledFunc = CI->getCalledFunction();
+    if(CalledFunc && CalledFunc->hasName()) {
+      const StringRef Name = CalledFunc->getName();
+      return LibcIO.find(Name) != LibcIO.end();
+    }
+    return false;
+  }
+
+  /// Search for & wrap libc functions which are likely to can an HTM abort
+  void wrapLibcWithHTM(Function &F) {
+    Instruction *Start, *End;
+
+    for(Function::iterator BB = F.begin(), BE = F.end(); BB != BE; BB++) {
+      for(BasicBlock::iterator I = BB->begin(), E = BB->end(); I != E; I++) {
+        if(isLibcIO(&*I)) {
+          // Wrap multiple consecutive I/O calls together
+          Start = End = &*I;
+          while(isLibcIO(End->getNextNode())) {
+            End = End->getNextNode();
+            I++;
+          }
+
+          IRBuilder<> Worker(Start);
+          Worker.CreateCall(getHTMAsm<HTMEnd>(*Start->getModule()));
+          Worker.SetInsertPoint(End->getNextNode());
+          Worker.CreateCall(getHTMAsm<HTMBegin>(*Start->getModule()));
+        }
+      }
+    }
   }
 };
 
@@ -184,16 +233,42 @@ private:
 
 char EquivalencePoints::ID = 0;
 
-const StringMap<EquivalencePoints::HTMAsmSpec> EquivalencePoints::HTMAsm = {
+// TODO PowerPC assembly
+const StringMap<EquivalencePoints::AsmSpec> EquivalencePoints::HTMBegin = {
+  {"x86_64", {"xbegin 1f;1:", "~{eax},~{dirflag},~{fpsr},~{flags}",
+              true, false, InlineAsm::AD_ATT}}
+};
+
+const StringMap<EquivalencePoints::AsmSpec> EquivalencePoints::HTMEnd = {
+  {"x86_64", {"xtest;jz 1f;xend;1:", "~{dirflag},~{fpsr},~{flags}",
+              true, false, InlineAsm::AD_ATT}}
+};
+
+const StringMap<EquivalencePoints::AsmSpec> EquivalencePoints::HTMEqPoint = {
   {"x86_64", {"xtest;jz 1f;xend;1:xbegin 2f;2:",
               "~{eax},~{dirflag},~{fpsr},~{flags}",
               true, false, InlineAsm::AD_ATT}}
-  // TODO PowerPC assembly
 };
 
 INITIALIZE_PASS(EquivalencePoints, "equivalence-points",
                 "Insert equivalence points into functions",
                 true, false)
+
+const StringSet<> EquivalencePoints::LibcIO = {
+  "fopen", "freopen", "fclose", "fflush", "fwide",
+  "setbuf", "setvbuf", "fread", "fwrite",
+  "fgetc", "getc", "fgets", "fputc", "putc", "fputs",
+  "getchar", "gets", "putchar", "puts", "ungetc",
+  "fgetwc", "getwc", "fgetws", "fputwc", "putwc", "fputws",
+  "getwchar", "putwchar", "ungetwc",
+  "scanf", "fscanf", "vscanf", "vfscanf",
+  "printf", "fprintf", "vprintf", "vfprintf",
+  "wscanf", "fwscanf", "vwscanf", "vfwscanf",
+  "wprintf", "fwprintf", "vwprintf", "vfwprintf",
+  "ftell", "fgetpos", "fseek", "fsetpos", "rewind",
+  "clearerr", "feof", "ferror", "perror",
+  "remove", "rename", "tmpfile", "tmpnam"
+};
 
 namespace llvm {
   FunctionPass *createEquivalencePointsPass()
