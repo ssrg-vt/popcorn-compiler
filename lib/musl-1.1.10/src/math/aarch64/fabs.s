c5443500bad1925aacd43f8938b4260c1a645df3
Pierre Olivier <polivier@vt.edu>
2017-08-15 18:27:04 -0400

    Took care of fabs for x86 and arm, will need to have compiler support for power before being able to make it work with that architecture. See comments I added in the code

--- lib/musl-1.1.10/src/math/aarch64/fabs.s
+++ lib/musl-1.1.10/src/math/aarch64/fabs.s
@@ -1,4 +1,12 @@
-.text
+/* Pierre: -ffunction-section obviously does not work with as so let's mimic
+ * its effect by hand. There is no power8 specific function for fabs, meaning
+ * it will probably use the dfault implementation in math/fabs.c. When power8 
+ * support is implemented we need to check what will be the name of the section
+ * in which -ffunction-sections will put fabs for power, and put the same name 
+ * here */
+
+.section .text.fabs, "ax"
+/* .text */
 .global fabs
 .type   fabs,%function
 fabs:
--- lib/musl-1.1.10/src/math/x86_64/fabs.s
+++ lib/musl-1.1.10/src/math/x86_64/fabs.s
@@ -1,3 +1,11 @@
+/* Pierre: -ffunction-section obviously does not work with as so let's mimic
+ * its effect by hand. There is no power8 specific function for fabs, meaning
+ * it will probably use the dfault implementation in math/fabs.c. When power8 
+ * support is implemented we need to check what will be the name of the section
+ * in which -ffunction-sections will put fabs for power, and put the same name 
+ * here */
+
+.section .text.fabs, "ax"
 .global fabs
 .type fabs,@function
 fabs:
