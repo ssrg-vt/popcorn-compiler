9beba159944926e4d0caef64ba79e59eab245c2e
Pierre Olivier <polivier@vt.edu>
2017-10-17 21:52:57 -0400

    Last version of the alignment tool integrated into PPC

---
+++ tool/alignment/pyalign/Power.py
@@ -0,0 +1,25 @@
+"""
+Class representing the power8 architecture specific attributes
+"""
+
+# TODO This file needs to be completed when the power compiler is working
+
+from AbstractArchitecture import AbstractArchitecture
+from Arch import Arch
+import Symbol
+
+class Power(AbstractArchitecture):
+
+	def __init__(self):
+		# Executable name
+		self._executable = ""
+		# Name of the map file output by gold
+		self._mapFile = ""
+		# Linker script template
+		self._linkerScriptTemplate = "ls_power.template"
+
+	def getArch(self):
+		return Arch.POWER
+
+	def getArchString(self):
+		return "POWER"
