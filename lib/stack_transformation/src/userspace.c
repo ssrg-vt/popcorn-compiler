52cd92ad8cb620d757ffc5dcc9cbe39ba0671466
Rob Lyerly <rlyerly@vt.edu>
2017-12-03 10:49:04 -0500

    Fixed non-initialization bug for rewriting metadata in stack transform library

--- lib/stack_transformation/src/userspace.c
+++ lib/stack_transformation/src/userspace.c
@@ -119,13 +119,10 @@ void __st_userspace_ctor(void)
   else {
     aarch64_fn = (char*)malloc(sizeof(char) * BUF_SIZE);
     snprintf(aarch64_fn, BUF_SIZE, "%s_aarch64", __progname);
-    aarch64_handle = st_init(aarch64_fn);
-    alloc_aarch64_fn = true;
   }
-
-  if(!aarch64_handle) {
-    ST_WARN("could not initialize aarch64 handle\n");
-  }
+  aarch64_handle = st_init(aarch64_fn);
+  if(aarch64_handle) alloc_aarch64_fn = true;
+  else { ST_WARN("could not initialize aarch64 handle\n"); }
 
   if(getenv(ENV_POWERPC64_BIN))
     powerpc64_handle = st_init(getenv(ENV_POWERPC64_BIN));
@@ -133,26 +130,20 @@ void __st_userspace_ctor(void)
   else {
     powerpc64_fn = (char*)malloc(sizeof(char) * BUF_SIZE);
     snprintf(powerpc64_fn, BUF_SIZE, "%s_powerpc64", __progname);
-    powerpc64_handle = st_init(powerpc64_fn);
-    alloc_powerpc64_fn = true;
   }
-
-  if(!powerpc64_handle) {
-    ST_WARN("could not initialize powerpc64 handle\n");
-  }
+  powerpc64_handle = st_init(powerpc64_fn);
+  if(powerpc64_handle) alloc_powerpc64_fn = true;
+  else { ST_WARN("could not initialize powerpc64 handle\n"); }
 
   if(getenv(ENV_X86_64_BIN)) x86_64_handle = st_init(getenv(ENV_X86_64_BIN));
   else if(x86_64_fn) x86_64_handle = st_init(x86_64_fn);
   else {
     x86_64_fn = (char*)malloc(sizeof(char) * BUF_SIZE);
     snprintf(x86_64_fn, BUF_SIZE, "%s_x86-64", __progname);
-    x86_64_handle = st_init(x86_64_fn);
-    alloc_x86_64_fn = true;
   }
-
-  if(!x86_64_handle) {
-    ST_WARN("could not initialize x86-64 handle\n");
-  }
+  x86_64_handle = st_init(x86_64_fn);
+  if(x86_64_handle) alloc_x86_64_fn = true;
+  else { ST_WARN("could not initialize x86-64 handle\n"); }
 }
 
 /*
