ffac48bca493a24038f7de0d7ad512a560eb04e3
Rob Lyerly <rlyerly@vt.edu>
2018-01-24 22:32:10 -0500

    Test case for page access trace/thread placement framework

---
+++ tool/page_access_trace/test/schedule-test.c
@@ -0,0 +1,94 @@
+/**
+ * Generate predictable page access patterns in order to sanity check the page
+ * analyis trace & thread placement framework.  There are several access
+ * patterms implemented, but each pattern has 4 threads sharing an individual
+ * page and hence those 4 threads should be placed together by the partitioning
+ * algorithm.
+ *
+ * Author: Rob Lyerly <rlyerly@vt.edu>
+ * Date: 1/24/2018
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <omp.h>
+#include <sys/sysinfo.h>
+
+/* Knobs controlling length of OpenMP sections & memory used */
+#define ITERS 2048L
+#define PAGES 1024L
+
+#define PAGESZ 4096L
+#define INTS_PER_PAGE (PAGESZ / sizeof(int))
+#define ARRSIZE (PAGES * INTS_PER_PAGE)
+#define CHUNKSZ (INTS_PER_PAGE / 4)
+
+#define NS( ts ) ((ts.tv_sec * 1000000000LU) + ts.tv_nsec)
+
+typedef int page_array_t [ARRSIZE];
+static page_array_t thearray;
+
+void randomize(page_array_t array) {
+  size_t i;
+  #pragma omp parallel for
+  for(i = 0; i < ARRSIZE; i++) array[i] = rand() % 1024;
+}
+
+/*
+ * Pattern 1: groups of 4 consecutive threads should be mapped to the same
+ * node.
+ */
+void add1(page_array_t array, const size_t iters) {
+  size_t iter, i;
+
+  printf("Region 1: consecutive threads access the same page...\n");
+
+  // TODO make this region 1
+  for(iter = 0; iter < iters; iter++) {
+    #pragma omp parallel for schedule(static, CHUNKSZ)
+    for(i = 0; i < ARRSIZE; i++) array[i] += 1;
+  }
+}
+
+/*
+ * Pattern 2: threads with the same parity should be mapped to the same node
+ * (e.g., evens with evens, odds with odds).
+ */
+void add2(page_array_t array, const size_t iters) {
+  size_t iter, i;
+
+  printf("Region 2: threads with the same parity access the same page...\n");
+
+  // TODO make this region 2
+  for(iter = 0; iter < iters; iter++) {
+    #pragma omp parallel
+    {
+      int thread = omp_get_thread_num();
+      long offset;
+      if(thread % 2) offset = (4 + (thread / 2) - thread) * CHUNKSZ;
+      else offset = -((thread / 2) * CHUNKSZ);
+
+      #pragma omp for schedule(static, CHUNKSZ)
+      for(i = 0; i < ARRSIZE; i++) array[i + offset] += 2;
+    }
+  }
+}
+
+int main(int argc, char** argv) {
+  size_t threads = get_nprocs_conf();
+  struct timespec start, end;
+
+  omp_set_num_threads(threads);
+  randomize(thearray);
+
+  printf("--------------------\nTHREAD SCHEDULE TEST\n--------------------\n");
+  printf("Running %lu iterations with %lu threads...\n", ITERS, threads);
+
+  clock_gettime(CLOCK_MONOTONIC, &start);
+  add1(thearray, ITERS);
+  add2(thearray, ITERS);
+  clock_gettime(CLOCK_MONOTONIC, &end);
+
+  printf("Total execution time: %lu ms\n", (NS(end) - NS(start)) / 1000000);
+}
+
