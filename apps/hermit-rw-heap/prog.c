45e5fc93fd5cfad05ec273bcb1a2efcd6e39ec5b
Pierre Olivier <polivier@vt.edu>
2018-04-09 11:06:43 -0400

    CLeanup & test integration

---
+++ apps/hermit-rw-heap/prog.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#define BINARY "./prog_x86-64"
+
+__thread int x = 24;
+__thread int y;
+
+int main(int argc, char **argv) {
+	
+	int fd2;
+
+	char *buf4 = malloc(250*1024*1204);
+	memset(buf4, 0x0, 250*1024*1024);
+
+	char * buf3 = malloc(8192);
+	memset(buf3, 0x0, 8192);
+
+	printf("hi\n");
+
+	fd2 = open(BINARY, O_RDONLY, 0x0);
+	if(fd2 == -1) {
+		printf("error open\n");
+		return -1;
+	}
+	
+
+	char *buf = malloc(4096*4);
+	if(!buf) {
+		fprintf(stderr, "error malloc!\n");
+		assert(0);
+	}
+	memset(buf, 0x0, 4096*4);
+	printf("malloc returned %p\n", buf);
+
+	read(fd2, buf, 4096*4);
+	printf("read:\n");
+	for(int i=0; i<4; i++)
+		printf("%c", buf[i]);
+	printf("\n");
+
+
+	free(buf4);
+
+	close(fd2);
+	printf("bye!\n");
+	return 0;
+}
