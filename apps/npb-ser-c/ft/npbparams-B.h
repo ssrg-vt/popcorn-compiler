67d4f13adf849d1e5b1a7465e28cbc594b46d5de
Pierre Olivier <polivier@vt.edu>
2018-04-11 12:22:06 -0400

    Better makefile template to handle multiple source files + NPB ser sources and compatibility list

---
+++ apps/npb-ser-c/ft/npbparams-B.h
@@ -0,0 +1,26 @@
+/* CLASS = B */
+/*
+   This file is generated automatically by the setparams utility.
+   It sets the number of processors and the class of the NPB
+   in this directory. Do not modify it by hand.   
+*/
+#define NX             512
+#define NY             256
+#define NZ             256
+#define MAXDIM         512
+#define NITER_DEFAULT  20
+#define NXP            513
+#define NYP            256
+#define NTOTAL         33554432
+#define NTOTALP        33619968
+
+#define CONVERTDOUBLE  false
+#define COMPILETIME "03 Nov 2017"
+#define NPBVERSION "3.3.1"
+#define CS1 "gcc"
+#define CS2 "$(CC)"
+#define CS3 "-lm"
+#define CS4 "-I../common"
+#define CS5 "-g -Wall -O3 -mcmodel=medium"
+#define CS6 "-O3 -mcmodel=medium"
+#define CS7 "randdp"
---
+++ apps/npb-ser-c/ua/npbparams-A.h
@@ -0,0 +1,33 @@
+/* CLASS = A */
+/*
+   This file is generated automatically by the setparams utility.
+   It sets the number of processors and the class of the NPB
+   in this directory. Do not modify it by hand.   
+*/
+#define LELT           2400
+
+#define LMOR           92700
+
+#define REFINE_MAX     6
+
+#define FRE_DEFAULT    5
+
+#define NITER_DEFAULT  200
+
+#define NMXH_DEFAULT   10
+
+#define CLASS_DEFAULT  'A'
+
+#define ALPHA_DEFAULT  0.076e0
+
+
+#define CONVERTDOUBLE  false
+#define COMPILETIME "03 Nov 2017"
+#define NPBVERSION "3.3.1"
+#define CS1 "gcc"
+#define CS2 "$(CC)"
+#define CS3 "-lm"
+#define CS4 "-I../common"
+#define CS5 "-g -Wall -O3 -mcmodel=medium"
+#define CS6 "-O3 -mcmodel=medium"
+#define CS7 "randdp"
