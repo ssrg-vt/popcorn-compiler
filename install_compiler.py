390b34513ba15fe5b650bc22e4fd7dbe28ed8f65
Pierre Olivier <polivier@vt.edu>
2018-06-26 19:59:23 -0400

    Add debug options for hermti aarch64

--- install_compiler.py
+++ install_compiler.py
@@ -741,6 +741,7 @@ def install_hermit_aarch64(base_path, install_path, threads):
         rv = subprocess.check_call(['cmake', '-DHERMIT_ARCH=aarch64', '-DCMAKE_INSTALL_PREFIX=%s' %
             install_path, '-DCOMPILER_BIN_DIR=%s' % install_path +
             '/x86_64-host/bin', '-DHERMIT_PREFIX=%s' % install_path + '/x86_64-host',
+            '-DMIGRATION_LOG=1', '-DKERNEL_DEBUG=1', #TODO remove debug options or make it optional at some point
             '..'])
     except Exception as e:
         sys.exit('Error running hermitcore aarch64 cmake: {}'.format(e))
