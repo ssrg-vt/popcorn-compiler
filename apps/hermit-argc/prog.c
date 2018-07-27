ea5bf02d1b992626203592b5937839e2f77b4e93
Pierre Olivier <polivier@vt.edu>
2018-07-27 18:39:50 -0400

    hermit argc/envc tests

---
+++ apps/hermit-argc/prog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+extern char **environ;
+
+int main(int argc, char **argv) {
+	int i;
+
+	printf("argc: %d\n", argc);
+	for(i=0; i<argc; i++) {
+		printf("  argv[%d]: %s\n", i, argv[i]);
+	}
+
+	printf("environ: %x\n", environ);
+	i = 0;
+	while(environ[i] != NULL)
+		printf("  %s\n", environ[i++]);
+
+	printf("test done\n");
+
+	return 0;
+}
