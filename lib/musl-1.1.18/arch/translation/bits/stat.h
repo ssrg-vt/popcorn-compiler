538d0a42ae5c3f57e015c86566a3ade50f65d927
SengMing Yeoh <sengming@.vt.edu>
2019-02-14 10:16:56 -0500

    Forgot to stage the translation layers to previous commit

---
+++ lib/musl-1.1.18/arch/translation/bits/stat.h
@@ -0,0 +1,15 @@
+struct stat {
+	dev_t st_dev;
+	ino_t st_ino;
+	mode_t st_mode;
+	nlink_t st_nlink;
+	uid_t st_uid;
+	gid_t st_gid;
+	dev_t st_rdev;
+	off_t st_size;
+	blksize_t st_blksize;
+	blkcnt_t st_blocks;
+	struct timespec st_atim;
+	struct timespec st_mtim;
+	struct timespec st_ctim;
+};
