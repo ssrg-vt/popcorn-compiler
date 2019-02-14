06961a12a35c9b30bb30b10e598192de5701c6d8
SengMing Yeoh <sengming@.vt.edu>
2019-02-14 10:04:01 -0500

    Added fstat stat.h in translation layer. Modified translation layer so stat structs are the same size. Updated linking within musl and install to target so the header files in the translation directory are propogated to both archs

--- lib/musl-1.1.18/src/stat/fstat.c
+++ lib/musl-1.1.18/src/stat/fstat.c
@@ -1,4 +1,5 @@
 #include <sys/stat.h>
+#include <translated/stat.h>
 #include <errno.h>
 #include <fcntl.h>
 #include "syscall.h"
@@ -8,7 +9,25 @@ void __procfdname(char *, unsigned);
 
 int fstat(int fd, struct stat *st)
 {
-	int ret = __syscall(SYS_fstat, fd, st);
+	struct stat_specific sts;
+	int ret = __syscall(SYS_fstat, fd, &sts);
+	/* Copy values over from arch specific stat to generic stat so include
+	 * from application side can use the translation layer irrespective of
+	 * architecture. There may be a better more generic way of doing this
+	 * but doing a direct member to member copy will suffice for now. */
+	st->st_dev = sts.st_dev;
+	st->st_ino = sts.st_ino;
+	st->st_mode = sts.st_mode;
+	st->st_nlink = sts.st_nlink;
+	st->st_uid = sts.st_uid;
+	st->st_gid = sts.st_gid;
+	st->st_rdev = sts.st_rdev;
+	st->st_size = sts.st_size;
+	st->st_blksize = sts.st_blksize;
+	st->st_blocks = sts.st_blocks;
+	st->st_atim = sts.st_atim;
+	st->st_mtim = sts.st_mtim;
+	st->st_ctim = sts.st_ctim;
 	if (ret != -EBADF || __syscall(SYS_fcntl, fd, F_GETFD) < 0)
 		return __syscall_ret(ret);
 
