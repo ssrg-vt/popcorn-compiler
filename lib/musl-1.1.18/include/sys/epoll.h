691acb1dfc3741ec1d0c77219446a8cffd9a2996
Cesar Philippidis <cesar@rasec.tech>
2020-11-11 18:44:36 -0800

    pack all epoll_data structures regardless of the arch

--- lib/musl-1.1.18/include/sys/epoll.h
+++ lib/musl-1.1.18/include/sys/epoll.h
@@ -47,11 +47,7 @@ typedef union epoll_data {
 struct epoll_event {
 	uint32_t events;
 	epoll_data_t data;
-}
-#ifdef __x86_64__
-__attribute__ ((__packed__))
-#endif
-;
+} __attribute__ ((__packed__));
 
 
 int epoll_create(int);
