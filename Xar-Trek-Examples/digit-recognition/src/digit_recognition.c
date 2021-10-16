85fe599f02223c43bd26fae87987b388dfefc565
ELHorta <44071690+ELHorta@users.noreply.github.com>
2021-10-15 23:51:41 -0400

    Add files via upload

--- Xar-Trek-Examples/digit-recognition/src/digit_recognition.c
+++ Xar-Trek-Examples/digit-recognition/src/digit_recognition.c
@@ -27,25 +27,12 @@
 
 
 
-// KNL HOST 0 - BEGIN - Include APIs and Migration Flag
-#include "KNL_Function.h"
-#include "KNL_Load_Exec.c"
-extern int per_app_migration_flag;
-// KNL HOST 0 - END - Include APIs and Migration Flag
-
-
 int main(int argc, char ** argv)
 {
   // timers
   struct timeval start, end;
   gettimeofday(&start, NULL);
 
-
-// KNL HOST 1 - BEGIN - Start Scheduler Client + INIT Kernel 
-popcorn_client(0);
-while (per_app_migration_flag == -1);
-if (per_app_migration_flag == 2) KNL_HW_INIT (context, commands, program, fpga_kernel);
-// KNL HOST 1 - END - Start Scheduler Client + INIT Kernel + 
 
 
 
@@ -64,23 +51,7 @@ if (per_app_migration_flag == 2) KNL_HW_INIT (context, commands, program, fpga_k
 
 
 
-// KNL HOST 2 - BEGIN - Popcorn Migration Options
-#ifdef PRINT_INFO
-printf("DBG---> Migration Flag: %d", per_app_migration_flag);
- #endif
-
-if (per_app_migration_flag == 0) { 
-
 		DigitRec_sw(training_data, testing_data, result);
-
-} else if (per_app_migration_flag == 1) {
-migrate (1,0,0);
-		DigitRec_sw(training_data, testing_data, result);
-migrate (0,0,0);
-} else if (per_app_migration_flag == 2) {
-KNL_HW_DigitRec_sw (context, commands, program, fpga_kernel, training_data, testing_data, result); 
-}
-// KNL HOST 2 - END - Popcorn Migration Options
 
 
   // check results
@@ -97,11 +68,6 @@ KNL_HW_DigitRec_sw (context, commands, program, fpga_kernel, training_data, test
   // print time
   long long elapsed = (end.tv_sec - start.tv_sec) * 1000000LL + end.tv_usec - start.tv_usec;
   printf("DigRec2000 \t\telapsed time: %lld us\n", elapsed);
-
-
-// KNL HOST 3 - BEGIN - Stop Scheduler Client
-popcorn_client(1);
-// KNL HOST 3 - END - Stop Scheduler Client
 
 
 
