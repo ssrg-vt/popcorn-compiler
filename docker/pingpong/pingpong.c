d6c0c14b5e39e4c2b2301691f0e2ba47abb63ba8
Xiaoguang Wang <xjtuwxg@gmail.com>
2022-02-28 14:51:27 -0500

    Add the pingpong test application to docker/.

---
+++ docker/pingpong/pingpong.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include "migrate.h"
+
+int tid_main;
+
+void func(int i)
+{
+	printf("[%d] (thread %d): Executing %s, on %s node.\n", i, gettid(),
+		__func__, gettid()==tid_main?"local":"remote");
+	sleep(2);
+	migrate(gettid()==tid_main ? 1:0, NULL, NULL);
+}
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	tid_main = gettid();
+	printf("+ ping pong hopping between two nodes\n");
+	printf("+ thread id on x86 node %d.\n", tid_main);
+
+	for (i = 0; i < 10; i++) {
+		func(i);
+	}
+	return 0;
+}
